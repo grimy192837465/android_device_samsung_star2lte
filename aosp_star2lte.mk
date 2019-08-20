db9b07063150d26ed9597b994d028113bf480a0a
Christopher A. Williamson <home@chrisaw.com>
2019-08-20 16:43:18 +0100

    Add comment present in other repos but not this one

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -7,5 +7,6 @@ PRODUCT_NAME := aosp_star2lte
 
 BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:9/PPR1.180610.011/G965FXXU4CSE3:user/release-keys
 
+# add fingerprint override to pass cts
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRIVATE_BUILD_DESC="star2ltexx-user 9 PPR1.180610.011 G965FXXU4CSE3 release-keys"
