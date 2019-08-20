2003cdec0294e883ab544d3da8ea98e5e9528467
Christopher A. Williamson <you@example.com>
2019-08-20 13:35:31 +0100

    Adapt devoice tree for AOSP based ROMs

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -3,7 +3,7 @@
 
 $(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
 
-PRODUCT_NAME := star2lte
+PRODUCT_NAME := aosp_star2lte
 
 BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:9/PPR1.180610.011/G965FXXU4CSE3:user/release-keys
 
